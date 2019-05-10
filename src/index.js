bc688d33658bca2f49b87ec084b7f0313f6b63c3
Rodrigo <rodrigo@infoservsoftware.com.br>
2019-05-10 18:01:33 -0300

    bug fix cors

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const io = require('socket.io')(server);
 
 const routes = require('./routes')({ io });
 
-app.use(routes);
 app.use(cors());
+app.use(routes);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
